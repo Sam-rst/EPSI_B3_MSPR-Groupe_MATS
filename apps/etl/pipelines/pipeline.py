4202d423680895784eaeecb3a3f9f45d7192a66b
Hiwayy <alexandre.pierre33130@gmail.com>
2025-04-05 15:00:57 +0200

    MSPR-58 Refacto de la pipeline supression de la def qui gerer les chemins predefinis car pas utilise

--- apps/etl/pipelines/pipeline.py
+++ apps/etl/pipelines/pipeline.py
@@ -10,40 +10,20 @@ class ETLPipeline:
         self.datasets = {}
         self.transformed_datasets = {}
     
-    def run(self, file_info_list):
-        """Execute le pipeline ETL complet sur une liste de fichiers"""
-        # Étape 1: Extraction - Charger les fichiers
-        self.datasets = self.exporter.load_files(file_info_list)
+    def run(self, input_files):
+        """Execute le pipeline ETL
+        
+        Args:
+            input_files: Peut être une liste d'objets FileInfo ou une liste de chemins (str)
+        """
+        if input_files and isinstance(input_files[0], str):
+            self.datasets = self.exporter.load_multiple_csv(input_files)
+        else:
+            self.datasets = self.exporter.load_files(input_files)
         
         if not self.datasets:
             print("Aucun dataset n'a été chargé. Le pipeline s'arrête.")
             return False
         
-        # Étape 2: Transformation - Nettoyer et standardiser les données
         self.transformed_datasets = self.transformer.transform_and_save(self.datasets)
-        
-        # Retourner le statut de succès
         return len(self.transformed_datasets) > 0
-    
-    def run_with_predefined_paths(self, file_paths):
-        """Execute le pipeline ETL avec des chemins de fichiers prédéfinis"""
-        # Étape 1: Extraction - Charger les fichiers
-        self.datasets = self.exporter.load_multiple_csv(file_paths)
-        
-        if not self.datasets:
-            print("Aucun dataset n'a été chargé. Le pipeline s'arrête.")
-            return False
-        
-        # Étape 2: Transformation - Nettoyer et standardiser les données
-        self.transformed_datasets = self.transformer.transform_and_save(self.datasets)
-        
-        # Retourner le statut de succès
-        return len(self.transformed_datasets) > 0
-    
-    def get_original_datasets(self):
-        """Renvoie les datasets originaux"""
-        return self.datasets
-    
-    def get_transformed_datasets(self):
-        """Renvoie les datasets transformés"""
-        return self.transformed_datasets
