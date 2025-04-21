b0c65d33c2f9fe9b979eba8527f94dd47313bf43
MaximeDussort <127834344+MaximeDussort@users.noreply.github.com>
2025-04-21 12:39:03 +0200

    Added Memory user Repository and User Container

---
+++ apps/api/src/app/user/container.py
@@ -0,0 +1,49 @@
+from dependency_injector import containers, providers
+
+from src.config.config import Config
+
+from src.app.user.infrastructure.repository.user_repo_in_memory import (
+    UserRepositoryInMemory,
+)
+from src.app.user.infrastructure.repository.user_repo_in_postgres import (
+    UserRepositoryInPostgres,
+)
+from src.app.user.application.usecase.add_user_usecase import AddUserUseCase
+from src.app.user.application.usecase.find_all_users_usecase import FindAllUsersUseCase
+from src.app.user.application.usecase.find_user_by_id_usecase import (
+    FindUserByIdUseCase,
+)
+from src.app.user.application.usecase.update_user_usecase import UpdateUserUseCase
+from src.app.user.application.usecase.delete_user_usecase import DeleteUserUseCase
+
+
+class UserContainer(containers.DeclarativeContainer):
+    modules = ["src.app.user.presentation.router"]
+    config = providers.Singleton(Config)
+
+    # Définir les repositories
+    repository_in_memory = providers.Singleton(UserRepositoryInMemory)
+    repository_in_postgres = providers.Singleton(UserRepositoryInPostgres)
+
+    # Sélectionner le repository en fonction de la configuration
+    repository = providers.Selector(
+        config.provided.REPOSITORY_TYPE,
+        in_memory=repository_in_memory,
+        in_postgres=repository_in_postgres,
+    )
+
+    # Définir les usecases
+    add_user_usecase = providers.Factory(AddUserUseCase, repository=repository)
+    find_all_users_usecase = providers.Factory(
+        FindAllUsersUseCase, repository=repository
+    )
+    find_user_by_id_usecase = providers.Factory(
+        FindUserByIdUseCase, repository=repository
+    )
+    update_user_usecase = providers.Factory(
+        UpdateUserUseCase,
+        repository=repository,
+    )
+    delete_user_usecase = providers.Factory(
+        DeleteUserUseCase, repository=repository
+    )
