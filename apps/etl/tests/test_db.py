2cf94e055f2296c68c00ce82778131d5fdfab772
Samuel RESSIOT <samuel.ressiot@gmail.com>
2025-04-26 12:43:51 +0200

    MSPR-99 : Modification de la pipeline CI + réorganisation de tous les projets de tests des services

--- apps/etl/tests/test_db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from config.settings import settings
-
-def test_database_connection():
-    engine = create_engine(settings.DATABASE_URL)
-    conn = engine.connect()
-    assert conn.closed is False
-    conn.close()
